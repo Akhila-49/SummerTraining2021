93238f0591ab2e70c0032389607ab18086d9c1af
Akhila-49 <85725678+Akhila-49@users.noreply.github.com>
2021-06-25 17:47:30 +0530

    Add files via upload

---
+++ webserver for docker/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+
+print("content-type: text/html")
+print()
+
+fs = cgi.FieldStorage()
+c = fs.getvalue("input")
+op = subprocess.getoutput("sudo " + c)
